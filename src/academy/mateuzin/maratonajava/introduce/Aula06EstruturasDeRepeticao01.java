5abdea8197f6cb71688d192ee84a260db0f001ae
Mateus Henrique <mateuzhenriq3@gmail.com>
2026-03-25 00:23:07 -0300

    feat: adicionando "for" no estudo

--- src/academy/mateuzin/maratonajava/introduce/Aula06EstruturasDeRepeticao01.java
+++ src/academy/mateuzin/maratonajava/introduce/Aula06EstruturasDeRepeticao01.java
@@ -15,5 +15,9 @@ public class Aula06EstruturasDeRepeticao01 {
             System.out.println("dentro do-while"+count);
             count++;
         } while (count < 10);
+
+        for (int i=0; i<10; i++) {
+            System.out.println("For: "+i);
+        }
     }
 }
