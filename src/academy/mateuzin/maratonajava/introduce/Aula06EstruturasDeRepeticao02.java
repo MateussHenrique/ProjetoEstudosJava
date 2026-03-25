085800c9ded2bf7625b7be56ccaa83376fdf4498
Mateus Henrique <mateuzhenriq3@gmail.com>
2026-03-25 00:30:32 -0300

    feat: adicionando classe de exercicio no estudo de Estruturas de Repetições

---
+++ src/academy/mateuzin/maratonajava/introduce/Aula06EstruturasDeRepeticao02.java
@@ -0,0 +1,12 @@
+package academy.mateuzin.maratonajava.introduce;
+
+public class Aula06EstruturasDeRepeticao02 {
+    public static void main(String[] args) {
+        //imprimir todos números pares de 0 até 100
+        int count = 0;
+
+        for (int i = 0; i <=100; i++){
+            System.out.println("Os números são: "+i);
+        }
+    }
+}
